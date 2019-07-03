5ea5161ac8242fc57504ab47006d44fcb1248c80
Simon McManus <mcmanus.simon@gmail.com>
2019-07-03 11:36:56 +0100

    comment out import script as ive been changing the posts in md format now

--- scripts/import-blog.js
+++ scripts/import-blog.js
@@ -1,59 +1,59 @@
 
-var fs = require('fs')
-var moment = require('moment')
-var chalk = require('chalk')
-var urlSafe = require('../lib/url-safe')
-var TurndownService = require('turndown')
+// var fs = require('fs')
+// var moment = require('moment')
+// var chalk = require('chalk')
+// var urlSafe = require('../lib/url-safe')
+// var TurndownService = require('turndown')
 
-var turndownService = new TurndownService({
-  codeBlockStyle: 'fenced'
-})
+// var turndownService = new TurndownService({
+//   codeBlockStyle: 'fenced'
+// })
 
-turndownService.addRule('pre', {
-  filter: ['pre', 'code'],
-  replacement: function (content) {
-    return '\n```js\n' + content + '\n```\n'
-  }
-})
+// turndownService.addRule('pre', {
+//   filter: ['pre', 'code'],
+//   replacement: function (content) {
+//     return '\n```js\n' + content + '\n```\n'
+//   }
+// })
 
-const path = require('path')
+// const path = require('path')
 
-const writeFile = function (fileName, contents) {
-  fileName = urlSafe(fileName)
-  const folder = path.dirname(fileName)
+// const writeFile = function (fileName, contents) {
+//   fileName = urlSafe(fileName)
+//   const folder = path.dirname(fileName)
 
-  console.log(folder)
-  fs.mkdir(__dirname + '/../posts/' + folder, { recursive: true }, function () {
-    const fullPath = __dirname + '/../posts/' + fileName + '.md'
+//   console.log(folder)
+//   fs.mkdir(__dirname + '/../posts/' + folder, { recursive: true }, function () {
+//     const fullPath = __dirname + '/../posts/' + fileName + '.md'
 
-    fs.writeFile(fullPath, contents, function (e, d) {
-      console.log(chalk.blue('Updating blog ', fullPath), e, d)
-      console.log(chalk.green('  ok'))
-    })
-  })
-}
+//     fs.writeFile(fullPath, contents, function (e, d) {
+//       console.log(chalk.blue('Updating blog ', fullPath), e, d)
+//       console.log(chalk.green('  ok'))
+//     })
+//   })
+// }
 
-var postSelectors = require('../posts').filter((post) => {
-  return (
-    post.title !== '' &&
-    post.created !== null &&
-    post.title !== null &&
-        post.content !== '' &&
-        post.title.slice(0, 10) !== 'links for '
-  )
-})
-  . forEach((post) => {
-    const year = moment(new Date(post.created)).format('YYYY/MM/DD')
-    const fileName = post.title
+// var postSelectors = require('../posts').filter((post) => {
+//   return (
+//     post.title !== '' &&
+//     post.created !== null &&
+//     post.title !== null &&
+//         post.content !== '' &&
+//         post.title.slice(0, 10) !== 'links for '
+//   )
+// })
+//   . forEach((post) => {
+//     const year = moment(new Date(post.created)).format('YYYY/MM/DD')
+//     const fileName = post.title
 
-    var markdown = turndownService.turndown(post.content.replace(/\n/g, '<br />'))
+//     var markdown = turndownService.turndown(post.content.replace(/\n/g, '<br />'))
 
-    const content = `---
-tags: ${post.tags || ''}
-title: ${post.title}
-date: ${post.created}
----
-${markdown}
-`
-    writeFile(fileName, content)
-  })
+//     const content = `---
+// tags: ${post.tags || ''}
+// title: ${post.title}
+// date: ${post.created}
+// ---
+// ${markdown}
+// `
+//     writeFile(fileName, content)
+//   })
