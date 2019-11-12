309d5b58edadef907c7f668104be01d11431215a
Simon McManus <mcmanus.simon@gmail.com>
2019-11-12 19:02:53 +0000

    tidy

--- components/link/link.js
+++ components/link/link.js
@@ -14,14 +14,12 @@ wickedElements.define('[is=smm-link]', Link)
 var html = {
     init: function(event) {
         this.el = event.currentTarget;
-        console.log('init', this.el)
-        this.render()
     },
     attributeFilter: ['data-speclate-page'],
+
     onattributechanged(event) {
         const {attributeName, oldValue, newValue} = event;
         console.log(attributeName, oldValue, newValue);
-        this.render()
     },
     render: () => { console.log('in render'); },
     onconnected:  function(event) { console.log(000) },
