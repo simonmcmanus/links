766f4859212d7590f37e4efb49b3cdabb2c88dd5
Simon McManus <mcmanus.simon@gmail.com>
2020-04-06 16:59:14 +0100

    use info instead of log for service worker registration

--- client/index.js
+++ client/index.js
@@ -25,9 +25,9 @@ if(window.location.host === 'simonmcmanus.com') {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
-    console.log('ServiceWorkerr registration successful with scope: ', registration.scope)
+    console.info('ServiceWorker registration successful with scope: ', registration.scope)
   }).catch(function (err) {
-    console.log('ServiceWorker registration failed: ', err)
+    console.info('ServiceWorker registration failed: ', err)
   })
 }
 
