b8ba2a17c17cbe098ef5dce8c50e0f79b1220e75
Simon McManus <mcmanus.simon@gmail.com>
2020-04-06 16:59:23 +0100

    fix service worker include

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,5 +1,5 @@
 var serviceWorker = require('speclate-service-worker')
-var offlineSpec = require('../offline-spec')
+var offlineSpec = require('./offline-spec')
 var version = '2.2'
 
 serviceWorker(offlineSpec, version)
