0a45f7080e958b9b32c2dce9ed42718db1966548
Simon McManus <mcmanus.simon@gmail.com>
2020-04-06 16:58:04 +0100

    removing service worker registration comments

--- client/index.js
+++ client/index.js
@@ -25,10 +25,8 @@ if(window.location.host === 'simonmcmanus.com') {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
-    // Registration was successful
     console.log('ServiceWorkerr registration successful with scope: ', registration.scope)
   }).catch(function (err) {
-    // registration failed :(
     console.log('ServiceWorker registration failed: ', err)
   })
 }
