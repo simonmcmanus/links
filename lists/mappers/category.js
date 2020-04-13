6ceac8dc5b7c03848aa4d9954f720df3809489dd
Simon McManus <mcmanus.simon@gmail.com>
2020-04-13 07:37:08 +0100

    tagging tweaks

--- lists/mappers/category.js
+++ lists/mappers/category.js
@@ -1,7 +1,12 @@
 module.exports = function (category) {
+    if(!category) {
+        return {
+            ".title": this.params.tags
+        }
+    }
 
     return {
-        ".title": category.title || this.params.tags || '',
+        ".title": category.title || '',
         ".summary": category.summary,
         "img": {
             src: category.image
--- lists/mappers/posts.js
+++ lists/mappers/posts.js
@@ -1,17 +1,22 @@
 module.exports = function (post) {
 
+    if(post === null) {
+        return {
+        }
+    }
+
     var post = {
         '.title': post.title,
         url: 'index.html',
         '.date': post.date,
     // '.dateUrl': moment(new Date(post.date)).format(urlFormat),
         '.summary': post.markup,
-        // '.tag':  post.tags.map((tag) => {
-        //     return {
-        //         "href": "/tags/" + tag + "/index.html",
-        //         "innerHTML": tag
-        //     }
-        // }) ,
+        '.tag':  post.tags.map((tag) => {
+            return {
+                "href": "/tags/" + tag + "/index.html",
+                "innerHTML": tag
+            }
+        }) ,
         'a.created': {
             //href: url,
             innerHTML: post.date
