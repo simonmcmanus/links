15c05d12dfd1b822d5824e63ae29a8d81d50d0c1
Simon McManus <mcmanus.simon@gmail.com>
2020-05-06 06:49:21 +0100

    removal

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+
+import resolve from '@rollup/plugin-node-resolve'
+
+export default 
+    {
+      input: 'client/index.js',
+      plugins: [
+        resolve({
+          customResolveOptions: {
+            moduleDirectory: 'node_modules'
+          }
+        }),
+      ],
+      output: {
+        file: './docs/client/index-compiled.js',
+        format: 'es'
+      },
+    }
+
+
