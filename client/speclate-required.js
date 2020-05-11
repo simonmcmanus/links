a76850800def1c0e97bb745047354194cd2ccc9c
Simon McManus <mcmanus.simon@gmail.com>
2020-05-11 19:49:54 +0100

    removed available for hire :)

---
+++ client/speclate-required.js
@@ -0,0 +1,34 @@
+export default {
+    "pages": [
+        "home",
+        "talks",
+        "hire",
+        "links",
+        "posts",
+        "tag"
+    ],
+    "components": [
+        "link",
+        "posts",
+        "post",
+        "category",
+        "posts-title",
+        "no-posts"
+    ],
+    "lists": [
+        "links",
+        "posts",
+        "categories"
+    ],
+    "filters": [
+        "mostRecent",
+        "byTitleSlug",
+        "byTags"
+    ],
+    "mappers": [
+        "links",
+        "posts",
+        "post",
+        "category"
+    ]
+}
