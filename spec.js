94e4524fa7b2eeb4e404896ac738c798ac10c833
Simon McManus <mcmanus.simon@gmail.com>
2020-05-24 07:24:03 +0100

    js pic

--- spec.js
+++ spec.js
@@ -172,7 +172,8 @@ module.exports = {
       "favicon.ico",
       "client/icons/play.svg",
       "client/icons/monitor.svg",
-      "client/global-styles-compiled.css"
+      "client/global-styles-compiled.css",
+      "client/assets/categories/js.png"
       
     ]
   }
