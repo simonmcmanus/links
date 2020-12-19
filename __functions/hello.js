0c4b43b0b8431d782c84096f8401f36960edeb1b
Simon McManus <mcmanus.simon@gmail.com>
2020-12-19 18:52:22 +0000

    TWITTER_

--- __functions/hello.js
+++ __functions/hello.js
@@ -14,14 +14,9 @@ var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-console.log({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
+
 
 
 const tweet = async(text) => {
