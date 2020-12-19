bf59e2301d39013f0a93d469a7f84c4c8937ff46
Simon McManus <mcmanus.simon@gmail.com>
2020-12-19 11:00:39 +0000

    do tweet

--- __functions/hello.js
+++ __functions/hello.js
@@ -1,6 +1,31 @@
 exports.handler = async function(event, context) {
+
+    tweet('test tweet')
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Hello World" })
     };
+}
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+
+
+const tweet = (text) => {
+    client.post('statuses/update', { status: text })
+        .then(function(tweet) {
+            console.log(tweet);
+        })
+        .catch(function(error) {
+            throw error;
+        })
+
+
 }
