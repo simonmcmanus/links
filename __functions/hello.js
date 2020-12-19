2fb6089220c50eef64526a1a61731ca760ac03ba
Simon McManus <mcmanus.simon@gmail.com>
2020-12-19 18:48:43 +0000

    debug

--- __functions/hello.js
+++ __functions/hello.js
@@ -16,7 +16,12 @@ var client = new Twitter({
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
-
+console.log({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
 
 
 const tweet = async(text) => {
