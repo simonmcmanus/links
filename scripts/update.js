a4033b9d725aba1a572afc4b9631a462030825fc
Simon McManus <mcmanus.simon@gmail.com>
2020-12-19 10:41:29 +0000

    make docs

--- scripts/update.js
+++ scripts/update.js
@@ -1,39 +1,39 @@
-// var superagent = require("superagent");
-// var fs = require("fs");
-// var moment = require("moment");
-// var chalk = require("chalk");
-// var urlSafe = require("../lib/url-safe");
-// var url = require("url");
+var superagent = require("superagent");
+var fs = require("fs");
+var moment = require("moment");
+var chalk = require("chalk");
+var urlSafe = require("../lib/url-safe");
+var url = require("url");
 
-// var Encoder = require("node-html-encoder").Encoder;
-// var encoder = new Encoder("entity");
-// var urlFormat = "YYYY-MM-DD";
+var Encoder = require("node-html-encoder").Encoder;
+var encoder = new Encoder("entity");
+var urlFormat = "YYYY-MM-DD";
 
-// superagent
-//     .get("https://5vu7ki44h5.execute-api.eu-west-2.amazonaws.com/dev/links")
-//     .end(function(error, data) {
+superagent
+    .get("https://5vu7ki44h5.execute-api.eu-west-2.amazonaws.com/dev/links")
+    .end(function(error, data) {
 
-//         if (!data)
-//             return
-//         var links = data.body
-//             .filter(link => {
-//                 return link.title, link.url;
-//             })
-//             .map(link => {
-//                 const tags = link.tags.split(",");
-//                 if (tags) {
-//                     link.tags = tags;
-//                 }
-//                 link.date = link.created.substring(0, 10);
-//                 return link;
-//             });
-//         fs.writeFile(
-//             __dirname + "/../lists/links.json",
-//             JSON.stringify(links, null, 4),
-//             function(e, d) {
-//                 if (e) throw e;
-//                 console.log(chalk.blue("Fetching latest links..."));
-//                 console.log(chalk.green("  ok"));
-//             }
-//         );
-//     });
+        if (!data)
+            return
+        var links = data.body
+            .filter(link => {
+                return link.title, link.url;
+            })
+            .map(link => {
+                const tags = link.tags.split(",");
+                if (tags) {
+                    link.tags = tags;
+                }
+                link.date = link.created.substring(0, 10);
+                return link;
+            });
+        fs.writeFile(
+            __dirname + "/../lists/links.json",
+            JSON.stringify(links, null, 4),
+            function(e, d) {
+                if (e) throw e;
+                console.log(chalk.blue("Fetching latest links..."));
+                console.log(chalk.green("  ok"));
+            }
+        );
+    });
