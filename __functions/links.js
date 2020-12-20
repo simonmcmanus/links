ac925ca2e56a836e384a18d5f59e47e21790119d
Simon McManus <mcmanus.simon@gmail.com>
2020-12-20 07:26:22 +0000

    load links file

--- __functions/links.js
+++ __functions/links.js
@@ -1,30 +1,43 @@
 const AWS = require("aws-sdk")
 
-// This function reads the contents of `name-of-your-file.json` from an
-// AWS S3 bucket and returns the contents.
+
+
 exports.handler = async(event, context) => {
+
+    const body = JSON.parse(event.body)
+
+    const input = {
+        create: new Date(),
+        url: body.url,
+        title: body.title,
+        summary: body.summary,
+        tags: body.tags
+    }
+
     const s3 = new AWS.S3({
         accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
     })
 
     try {
-        const result = await s3.getObject({
-            // The bucket name
-            Bucket: "smm_links",
-            // The key/name of your file
-            Key: `links.json`,
-        })
-        console.log(result.Body.toString('utf-8'))
-
-
-
-        return {
-            statusCode: 200,
-
-            body: JSON.stringify(result.body),
+        var params = {
+            Key: "links.json",
+            Bucket: 'netlify-files',
         }
+        const s3Objects = await s3.getObject(params).promise();
+
+        const links = JSON.parse(s3Objects.Body.toString('utf-8'))
+        links.push(input)
+
+        const updated = await s3.putObject({
+            Bucket: params.Bucket,
+            Key: params.Key,
+            Body: JSON.stringify(links, null, 4)
+        }).promise()
+
+        console.log('fin', updated)
     } catch (e) {
-        return { statusCode: 500, body: e.message }
+        console.log(e)
+            //return { statusCode: 500, body: e.message }
     }
 }
